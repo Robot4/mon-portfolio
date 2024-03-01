2d6c077459aebe401a383bbf60792be2267fb03a
MEHDI <mehdi.hail.33@gmail.com>
2024-03-01 13:31:11 +0100

    add navbar

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -40,25 +40,36 @@ const Banner = ({ hide, setHide }: BannerProps) => {
         <p className="flex items-center text-sm font-normal text-gray-500 ">
           <span className="inline-flex p-1 mr-3 bg-gray-200 rounded-full ">
             <img
-              src={bulb}
-              alt="bulb"
-              height={12}
-              width={12}
-              className="text-gray-500"
+                src={bulb}
+                alt="bulb"
+                height={12}
+                width={12}
+                className="text-gray-500"
             />
             <span className="sr-only">Light bulb</span>
           </span>
-
+          <span>
+           Merci pour votre viste {" "}
+            <Link
+                to=":)"
+                target="_blank"
+                rel="noreferrer noopener"
+                className="inline font-medium text-blue-600 underline underline-offset-2 decoration-600 hover:no-underline"
+            >
+              new portfolio
+            </Link>
+            .
+          </span>
         </p>
       </div>
       <div className="flex items-center">
         <button
-          data-dismiss-target="#sticky-banner"
-          onClick={handleBannerClose}
-          type="button"
-          className="flex-shrink-0 inline-flex justify-center items-center text-gray-400 hover:bg-gray-800 rounded-lg text-sm p-1.5 "
+            data-dismiss-target="#sticky-banner"
+            onClick={handleBannerClose}
+            type="button"
+            className="flex-shrink-0 inline-flex justify-center items-center text-gray-400 hover:bg-gray-800 rounded-lg text-sm p-1.5 "
         >
-          <img src={close} alt="close" height={12} width={12} />
+          <img src={close} alt="close" height={12} width={12}/>
           <span className="sr-only">Close banner</span>
         </button>
       </div>
